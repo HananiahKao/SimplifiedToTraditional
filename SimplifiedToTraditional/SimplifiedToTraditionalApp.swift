5b51e9ebaf83b633289055fb40570022b5d8a9ab
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-07-25 09:10:50 +0800

    Initial Commit

---
+++ SimplifiedToTraditional/SimplifiedToTraditionalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplifiedToTraditionalApp.swift
+//  SimplifiedToTraditional
+//
+//  Created by Hananiah on 2025/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SimplifiedToTraditionalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimplifiedToTraditionalTests/SimplifiedToTraditionalTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimplifiedToTraditionalTests.swift
+//  SimplifiedToTraditionalTests
+//
+//  Created by Hananiah on 2025/7/25.
+//
+
+import Testing
+@testable import SimplifiedToTraditional
+
+struct SimplifiedToTraditionalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
